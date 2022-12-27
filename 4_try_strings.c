0dd07c3ffeb8ed06ad894786b182f84e8fc28c05
chirag <chiraggulati098@gmail.com>
2022-12-27 16:52:49 +0530

    just learnt strings and made 2 c files today

---
+++ 4_try_strings.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char c[6] = {'H','e','l','l','o'};
+	char c2 [6] = "Hello";
+	printf("%s\n%s\n",c,c2);
+	return 0;
+}
---
+++ 5_try_puts_gets_in_strings.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[50];
+	printf("enter string: ");
+	gets(str);
+	puts(str);
+	return 0;
+}
